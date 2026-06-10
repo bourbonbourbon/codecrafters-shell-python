1baa754e6d4e53727c8cc8b0be97daa20f41891a
Abdul Rahman <Abdul Rahman>
2026-06-10 21:19:48 +0530

    feat: added 'type' shell builtin

--- app/main.py
+++ app/main.py
@@ -2,14 +2,26 @@ import sys
 
 
 def main():
+    _shell_builtins = ["echo", "exit", "type"]
     while True:
         sys.stdout.write("$ ")
-        command = input()
+        user_input = input()
+
+        command = user_input.split()[0]
+        args = user_input.split()[1:]
+
+
         if command == "exit":
             sys.exit(0)
 
-        elif command.split()[0] == "echo":
-            print(" ".join(command.split()[1:]))
+        elif command == "echo":
+            print(" ".join(args))
+
+        elif command == "type":
+            if args[0] in _shell_builtins:
+                print(f"{args[0]} is a shell builtin")
+            else:
+                print(f"{args[0]}: not found")
 
         else:
             print(f"{command}: command not found")
